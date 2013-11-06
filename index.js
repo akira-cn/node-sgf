2df203d1c79085112386445ee73de081d48c1b64
akira <akira.cn@gmail.com>
2013-11-06 15:46:30 +0800

    update

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-var Parser = require("jison").Parser;
-
-var grammar = {
-  "lex": {
-    "rules": [
-      ["\\s+",       "/* skip whitespace */"],
-      ["[A-Z]{1,2}", "return 'COMMAND';"],
-      ["\\[(?:\\\\.|[^\\\\\\]])*\\]", "return 'ARGUMENT';"],
-      [";",          "return 'BLOCK_MARKER';"],
-      ["\\(",        "return 'NODE_START';"],
-      ["\\)",        "return 'NODE_END';"],
-      ["$",          "return 'EOF';"]
-    ]
-  },  
-  "bnf": {
-    "game" :[[ "node EOF", "return $1;"  ]],
-    "node" :[[ "NODE_START blocks NODE_END",  "$$ = $2;" ]],
-    "blocks" :[[ "block blocks", "$2.unshift($1); $$ = $2;" ], [ "block", "$$ = [$1];" ]],
-    "block" :[[ "BLOCK_MARKER commands", "$$ = $2;"]],
-    "commands" : [[ "command commands", "$2.unshift($1); $$ = $2" ], ["command", "$$ = [$1];"]],
-    "command" : [[ "COMMAND arguments", "$$ = {command: $1, arguments: $2};" ]],
-    "arguments": [[ "ARGUMENT arguments", "$2.unshift($1.substring(1, $1.length-1)); $$ = $2" ], ["ARGUMENT", "$$ = [$1.substring(1, $1.length-1)];"]]
-  }
+function parse(data) {
+  return require('./lib/parser').parse(data);
 };
 
-var parser = new Parser(grammar);
+module.exports = parse;
 
-module.exports = function(data) {
-  return parser.parse(data);
-};
+/*
+var fs   = require('fs');
+var tree = parse(fs.readFileSync(__dirname + '/test/sample.sgf').toString());
+console.log(tree[0][0]['GM'][0]);
+*/
